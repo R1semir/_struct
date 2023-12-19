58889418349b3f74330fb6605a8b51deb7155f80
Emir <emirkan.ulker9@gmail.com>
2023-12-19 15:33:41 +0300

    v2

---
+++ 2struct2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2struct2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        struct bilgi
+        {
+            public string Sehir;
+            public int Plaka;
+            public string Unlu;
+            public string Rengi;
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bilgi blg = new bilgi();
+            blg.Sehir = textBox1.Text;
+            blg.Plaka = Convert.ToInt16(textBox2.Text);
+            blg.Unlu = textBox3.Text;
+            blg.Rengi = textBox4.Text;
+
+            MessageBox.Show("Şehir: " + blg.Sehir + "\nPlaka: " + blg.Plaka + "\nÜnlü Noktası: " + blg.Unlu + "\nŞehri Temsil Eden Renk: " + blg.Rengi) ;
+
+
+        }
+    }
+}
